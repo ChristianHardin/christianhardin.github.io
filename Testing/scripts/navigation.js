c8a004bde4b07d86807f0d9d678ae0a032af0fc1
christianhardin <christianhardin12345@gmail.com>
2023-03-07 19:47:02 -0500

    feat: Created new layout in testing foldier.
    Create the new basic color cheme (Tokyo Nights) and layout of the website. Also added JQuery functionality for dynamic navigation links.

--- Testing/scripts/navigation.js
+++ Testing/scripts/navigation.js
@@ -2,18 +2,17 @@
 $(document).ready(function() {
     console.log("JQuery Loaded");
     const pathname = String(location.pathname.split("/").pop());
-    $.getJSON("../data/navigationLinks.json", function(data) {
+    $.getJSON("./data/navigation.json", function(data) {
         const navigationObject = data;
+        // let htmlString = "";
         let htmlString = "<ul>";
 
         for (let i = 0; i < navigationObject.length; i++) {
-            htmlString += '<li><a href="' + navigationObject[i].url + '">' + navigationObject[i].text + '</a></li>';
+            htmlString += '<li><a href="' + navigationObject[i].url + '"><img src="'+ navigationObject[i].icon +'" alt="'+navigationObject[i].iconAlt +'"></a></li>';
         }
         htmlString += "</ul>";
         console.log(htmlString)
 
-        $('#navigationLinks').html(htmlString);
+        $('#sideNavigationLinks').html(htmlString);
     });
-
-    // $('quizContent').html('<p> suck </p>');
 });

dd8ff9fdf892d330cd096b82ce456ca0bf6a2047
christianhardin <christianhardin12345@gmail.com>
2023-03-09 14:41:52 -0500

    feat: Finished basic navigation script.
    Finished the basic navigation bar script in order to have a working side nav bar.

--- Testing/scripts/navigation.js
+++ Testing/scripts/navigation.js
@@ -1,18 +1,38 @@
 // Navigation Bar
 $(document).ready(function() {
     console.log("JQuery Loaded");
+    $.fn.navigationTextOn(false);
+
+    $("#sideNavigationLinks").hover(function() {
+        // On mouseOn
+        $(this).css("width", "10rem");
+        $.fn.navigationTextOn(true);
+    }, function() {
+        // On mouseOff
+        $(this).css("width", "3rem");
+        $.fn.navigationTextOn(false);
+    });
+});
+
+// Function To handle side bar navigation
+$.fn.navigationTextOn = function(text){ 
     const pathname = String(location.pathname.split("/").pop());
     $.getJSON("./data/navigation.json", function(data) {
         const navigationObject = data;
-        // let htmlString = "";
         let htmlString = "<ul>";
 
         for (let i = 0; i < navigationObject.length; i++) {
-            htmlString += '<li><a href="' + navigationObject[i].url + '"><img src="'+ navigationObject[i].icon +'" alt="'+navigationObject[i].iconAlt +'"></a></li>';
+            if (!text) {
+                htmlString += '<li><a href="' + navigationObject[i].url + '"><img src="'+ navigationObject[i].icon +'" alt="'+navigationObject[i].iconAlt +'" width=45 height=45></a></li>';
+            } else {
+                htmlString += '<li><a href="' + navigationObject[i].url + '"><img src="'+ navigationObject[i].icon +'" alt="'+navigationObject[i].iconAlt +'" width=45 height=45>' + navigationObject[i].text + '</a></li>';
+            }
         }
         htmlString += "</ul>";
-        console.log(htmlString)
-
         $('#sideNavigationLinks').html(htmlString);
     });
-});
+}
+
+
+
+

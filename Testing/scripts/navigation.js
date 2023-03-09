07ceabbb8b3e1634c8e76c567f85b97b25f5abf8
christianhardin <christianhardin12345@gmail.com>
2023-03-09 18:14:26 -0500

    feat: Added Some Formatting

--- Testing/scripts/navigation.js
+++ Testing/scripts/navigation.js
@@ -2,7 +2,6 @@
 $(document).ready(function() {
     console.log("JQuery Loaded");
     $.fn.navigationTextOn(false);
-
     $("#sideNavigationLinks").hover(function() {
         // On mouseOn
         $(this).css("width", "10rem");
@@ -15,17 +14,17 @@ $(document).ready(function() {
 });
 
 // Function To handle side bar navigation
-$.fn.navigationTextOn = function(text){ 
+$.fn.navigationTextOn = function(textBool){ 
     const pathname = String(location.pathname.split("/").pop());
     $.getJSON("./data/navigation.json", function(data) {
         const navigationObject = data;
         let htmlString = "<ul>";
 
         for (let i = 0; i < navigationObject.length; i++) {
-            if (!text) {
+            if (!textBool) {
                 htmlString += '<li><a href="' + navigationObject[i].url + '"><img src="'+ navigationObject[i].icon +'" alt="'+navigationObject[i].iconAlt +'" width=45 height=45></a></li>';
             } else {
-                htmlString += '<li><a href="' + navigationObject[i].url + '"><img src="'+ navigationObject[i].icon +'" alt="'+navigationObject[i].iconAlt +'" width=45 height=45>' + navigationObject[i].text + '</a></li>';
+                htmlString += '<li><a href="' + navigationObject[i].url + '"><img src="'+ navigationObject[i].icon +'" alt="'+navigationObject[i].iconAlt +'" width=45 height=45><span id="iconText">' + navigationObject[i].text + '</span></a></li>';
             }
         }
         htmlString += "</ul>";

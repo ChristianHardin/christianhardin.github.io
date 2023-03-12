1d3b017a8cf4905df09fcfdaad8994a3761bdce2
christianhardin <christianhardin12345@gmail.com>
2023-03-11 20:55:05 -0500

    feat: Added dynamic bottom navigation
    Added the ability for the navigation.js to handle bottom navigation links.

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $.fn.navigationTextOn(false);
 
     let isMouseHovering = false
-    $("#sideNavigationLinks").hover(function() {
+    $("#mainNavigationLinks").hover(function() {
         // On mouseOn
         if (!isMouseHovering) {
             $.fn.navigationTextOn(true);
@@ -26,17 +26,23 @@ $.fn.navigationTextOn = function(textBool){
     const pathname = String(location.pathname.split("/").pop());
     $.getJSON("./data/navigation.json", function(data) {
         const navigationObject = data;
-        let htmlString = "<ul>";
+        let mainHtmlString = "<ul>";
+        let altHtmlString = "<ul>";
 
         for (let i = 0; i < navigationObject.length; i++) {
-            if (!textBool) {
-                htmlString += '<li><a href="' + navigationObject[i].url + '"><img src="'+ navigationObject[i].icon +'" alt="'+navigationObject[i].iconAlt +'" width=45 height=45></a></li>';
+            if (navigationObject[i].type == "mainNavigation") {
+                if (!textBool) {
+                    mainHtmlString += '<li><a href="' + navigationObject[i].url + '"><img src="'+ navigationObject[i].icon +'" alt="'+navigationObject[i].iconAlt +'" width=45 height=45></a></li>';
+                } else {
+                    mainHtmlString += '<li><a href="' + navigationObject[i].url + '"><img src="'+ navigationObject[i].icon +'" alt="'+navigationObject[i].iconAlt +'" width=45 height=45><span id="iconText">' + navigationObject[i].text + '</span></a></li>';
+                }
             } else {
-                htmlString += '<li><a href="' + navigationObject[i].url + '"><img src="'+ navigationObject[i].icon +'" alt="'+navigationObject[i].iconAlt +'" width=45 height=45><span id="iconText">' + navigationObject[i].text + '</span></a></li>';
+                altHtmlString += '<li><a href="' + navigationObject[i].url + '">' + navigationObject[i].text + '</a></li>';
             }
         }
-        htmlString += "</ul>";
-        $('#sideNavigationLinks').html(htmlString);
+        mainHtmlString += "</ul>";
+        $('#mainNavigationLinks').html(mainHtmlString);
+        $('#altNavigationLinks').html(altHtmlString);
     });
 }
 

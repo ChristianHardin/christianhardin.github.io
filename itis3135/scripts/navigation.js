96f150ad877ad1aa87b71f6d4b995ae2a6152818
christianhardin <christianhardin12345@gmail.com>
2023-03-20 21:04:39 -0400

    feat: Navbar redone. Animations Added.

--- itis3135/scripts/navigation.js
+++ itis3135/scripts/navigation.js
@@ -3,17 +3,18 @@ $(document).ready(function() {
     onPageLoad();
 
     $('#sideNavigation').on('click', '#categoryMain', function() {
-        testFunction("schoolWorkMenu");
+        menuManager("schoolWorkMenu");
     });
 
     $('#sideNavigation').on('click', '#categorySub', function() {
-        testFunction('miscWorkMenu');
+        menuManager('miscWorkMenu');
     });
 });
 
+let dataObj = "schoolWorkMenu";
 function onPageLoad() {
     $.getJSON("./data/navigation-category.json", function(data) {
-        const dataObj = data;
+        dataObj = data;
         let htmlString = '<div id="categoryWrapper"><div id="logo"><img src="https://placehold.jp/41x40.png" alt="bleh"></div><div class="line01"></div>';
 
         for (let i = 0; i < dataObj.length; i++) {
@@ -25,12 +26,12 @@ function onPageLoad() {
         }
         htmlString += '</div><div id="schoolWorkMenu"></div><div id="miscWorkMenu"></div>';
         $('#sideNavigation').html(htmlString);
-        testFunction("schoolWorkMenu");
+        menuManager("schoolWorkMenu");
     });
 }
 
 let currentMenu = "null";
-function testFunction(menuName) {
+function menuManager(menuName) {
     if (menuName == currentMenu) {
         destroyMenu(menuName);
     } else {
@@ -39,29 +40,33 @@ function testFunction(menuName) {
     }
 }
 
+
 function fillMenu(menuName) {
     $.getJSON("./data/navigation-" + menuName +".json", function(data) {
-        const dataObj = data;
+        dataObj = data;
         let htmlString = "<div id='menuTitle'><h1>" + dataObj[0].title + "</h1></div><div class='line01'></div><div id='linkWrapper'>";
         for (let i = 1; i < dataObj.length; i++) {
             htmlString += "<a class='menuLink' id='" + dataObj[i].id + "' href='" + dataObj[i].url + "'><img class='menuLinkIcon' src='" + dataObj[i].icon + "'><p class='menuLinkText'>" + dataObj[i].text + "</p></a>"
         }
         htmlString += '</div>'
         $('#sideNavigation').children().each(function() {
-            if ($(this).attr('id') == menuName) {
-                $(this).css("width", "12rem");
-                $(this).html(htmlString);
-            }
+            let object = $(this)
+            setTimeout(function() {
+                if (object.attr('id') == menuName) {
+                    object.animate({"width": "12rem"}, 100)
+                            .html(htmlString);
+                }
+              }, 100);
         });
     });
     currentMenu = menuName;
-} 
+}
 
 function destroyMenu(menuName) {
     let htmlString = '';
     $('#sideNavigation').children().each(function() {
             if ($(this).attr('id') == menuName) {
-                $(this).css("width", "0rem")
+                $(this).animate({"width": "0"}, 100)
                        .html(htmlString);
             }
     });
